4729fdd840f3c333fe5fa468b4ec907d4941c43a
Shelley Davis-Wise <shelleydavis-wise@Shelleys-MacBook-Air.local>
2015-09-09 11:55:48 -0500

    incorporated function to determine win

--- script.js
+++ script.js
@@ -15,9 +15,14 @@ var $nine =  $("#nine");
 
 function determineWinningCombo(box1, box2, box3, color) {
     if (box1.css("background-color") === color && box2.css("background-color") === color && box3.css("background-color") === color){
-        alert("You win");
+        if ( color === $red) {
+            $("table").append("<div> Red wins!</div>").css({"font-size": "72px", "background-color": "yellow"});
+         
+        }  else {
+            alert("Blue wins!");
+        }
     }   
-
+}
 function winningCombo() {
     determineWinningCombo($one, $two, $three, $red);
     determineWinningCombo($four, $five, $six, $red);
@@ -25,53 +30,18 @@ function winningCombo() {
     determineWinningCombo($one, $four, $seven, $red);
     determineWinningCombo($two, $five, $eight, $red);
     determineWinningCombo($three, $six, $nine, $red);
-    determineWinningCombo($one, $five, $nine, red);
-    determineWinningCombo($three, $five, $seven, red);
-    determineWinningCombo($one, $two, $three, blue);
-    determineWinningCombo($four, $five, $six, blue);
-    determineWinningCombo($seven, $eight, $nine, blue);
-    determineWinningCombo($one, $four, $seven, blue);
-    determineWinningCombo($two, $five, $eight, blue);
-    determineWinningCombo($three, $six, $nine, blue);
-    determineWinningCombo($one, $five, $nine, blue);
-    determineWinningCombo($three, $five, $seven, blue);
+    determineWinningCombo($one, $five, $nine, $red);
+    determineWinningCombo($three, $five, $seven, $red);
+    determineWinningCombo($one, $two, $three, $blue);
+    determineWinningCombo($four, $five, $six, $blue);
+    determineWinningCombo($seven, $eight, $nine, $blue);
+    determineWinningCombo($one, $four, $seven, $blue);
+    determineWinningCombo($two, $five, $eight, $blue);
+    determineWinningCombo($three, $six, $nine, $blue);
+    determineWinningCombo($one, $five, $nine, $blue);
+    determineWinningCombo($three, $five, $seven, $blue);
   }
-    // function winningCombo() {
-
-    // if ($( "#one").css("background-color") === red && $("#two").css("background-color") === red && $("#three").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#four").css("background-color") === red && $("#five").css("background-color") === red && $("#six").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#seven").css("background-color") === red && $("#eight").css("background-color") === red && $("#nine").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#one").css("background-color") === red && $("#four").css("background-color") === red && $("#seven").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#two").css("background-color") === red && $("#five").css("background-color") === red && $("#eight").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#three").css("background-color") === red && $("#six").css("background-color") === red && $("#nine").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#one").css("background-color") === red && $("#five").css("background-color") === red && $("#nine").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#three").css("background-color") === red && $("#five").css("background-color") === red && $("#seven").css("background-color") === red){
-    //     alert("Red wins!");}
-    // if ($("#one").css("background-color") === blue && $("#two").css("background-color") === blue && $("#three").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // if ($("#four").css("background-color") === blue && $("#five").css("background-color") === blue && $("#six").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // if ($("#seven").css("background-color") === blue && $("#eight").css("background-color") === blue && $("#nine").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // if ($("#one").css("background-color") === blue && $("#four").css("background-color") === blue && $("#seven").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // if ($("#two").css("background-color") === blue && $("#five").css("background-color") === blue && $("#eight").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // if ($("#three").css("background-color") === blue && $("#six").css("background-color") === blue && $("#nine").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // if ($("#one").css("background-color") === blue && $("#five").css("background-color") === blue && $("#nine").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // if ($("#three").css("background-color") === blue && $("#five").css("background-color") === blue && $("#seven").css("background-color") === blue){
-    //      alert("Blue wins!");}
-    // }
-
+    
 $(document).ready(function () {
 
 		$("td").on("click", function(){
@@ -94,5 +64,5 @@ $(document).ready(function () {
 		  }
 		}
     })
-)}
+})
 
